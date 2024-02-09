77c7de0a2feba4a4c7158ca500ea094eade9062b
Alen Scaria Antony <63664995+alenscaria@users.noreply.github.com>
2024-02-09 14:58:20 +0530

    Implemented CRUD Functionality

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price {  get; set; }
+        // public int Ratings { get; set; }
+
+
+    }
+
+    public class MovieDBContext : DbContext  // class represents the EF movie db context, which handles fetching, storing, and updating Movie class instances in a db. 
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
