6a1f8594e6ad8f40eca0b048b78095dbebe99003
Alen Scaria Antony <63664995+alenscaria@users.noreply.github.com>
2024-02-09 16:09:35 +0530

    Added Design

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -8,10 +8,7 @@ namespace MvcMovie.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        
 
         public ActionResult About()
         {
